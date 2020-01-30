371ab095d508bd8c1f084ab4710d0959dca0b9b5
stump26 <moidpg@gmail.com>
2020-01-31 00:32:20 +0900

    🚩Implement crawling at blitz.gg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 
-const opggHeadless = async (champion = "leesin") => {
+const opggHeadless = async (champion = "LeeSin") => {
   const getDOMElementSize = selector => {
     const element = document.querySelector(selector);
     if (!element) return null;
@@ -9,48 +9,72 @@ const opggHeadless = async (champion = "leesin") => {
   };
 
   const padding = "10";
-  const runeQuerySelector = ".champion-overview__table--rune";
-
+  const runeQuerySelector =
+    "#scroll-view-main > div > div.withNav__Wrapper-sc-1gdc90q-1.fQLInq > div > div > div.View-sc-1c57lgy-1.cLLSJv.View__StyledDiv-sc-1c57lgy-0.eidRwp > div:nth-child(3) > div.View-sc-1c57lgy-1.cLLSJv.View__StyledDiv-sc-1c57lgy-0.hxNeHv > div.Column-sc-1cxsa6a-0.ixlThO > div:nth-child(1) > div.Inner-sc-7vmxjm-0.htsreA > div:nth-child(1) > div.View-sc-1c57lgy-1.sc-jTqLGJ.iIRNkD.View__StyledDiv-sc-1c57lgy-0.eidRwp > div";
+  const itemAndSpellQuerySelector =
+    "#scroll-view-main > div > div.withNav__Wrapper-sc-1gdc90q-1.fQLInq > div > div > div.View-sc-1c57lgy-1.cLLSJv.View__StyledDiv-sc-1c57lgy-0.eidRwp > div:nth-child(3) > div.View-sc-1c57lgy-1.cLLSJv.View__StyledDiv-sc-1c57lgy-0.hxNeHv > div.Column-sc-1cxsa6a-0.ixlThO > div:nth-child(1) > div.Inner-sc-7vmxjm-0.htsreA > div:nth-child(1) > div.View-sc-1c57lgy-1.cLLSJv.View__StyledDiv-sc-1c57lgy-0.eidRwp";
   const browser = await puppeteer.launch({
     headless: true
   });
   const page = await browser.newPage();
 
-  // const defaultViewport = {
-  //   height: 1920,
-  //   width: 1280
-  // };
+  const defaultViewport = {
+    height: 1920,
+    width: 1280
+  };
 
-  await page.goto(`https://www.op.gg/champion/${champion}`, {
-    waitUntil: "networkidle0"
-  });
+  await page.goto(`https://blitz.gg/lol/champions/${champion}`);
 
-  // // view port size
-  // const bodyHandle = await page.$("body");
-  // const boundingBox = await bodyHandle.boundingBox();
-  // const newViewport = {
-  //   width: Math.max(defaultViewport.width, Math.ceil(boundingBox.width)),
-  //   height: Math.max(defaultViewport.height, Math.ceil(boundingBox.height))
-  // };
-  // await page.setViewport(Object.assign({}, defaultViewport, newViewport));
+  // Resize view-port for suitable content.
+  const bodyHandle = await page.$("body");
+  const boundingBox = await bodyHandle.boundingBox();
+  const newViewport = {
+    width: Math.max(defaultViewport.width, Math.ceil(boundingBox.width)),
+    height: Math.max(defaultViewport.height, Math.ceil(boundingBox.height))
+  };
+  await page.setViewport(Object.assign({}, defaultViewport, newViewport));
 
-  const rect = await page.evaluate(getDOMElementSize, runeQuerySelector);
-  console.log("TCL: opggHeadless -> rect", rect);
+  async function screenshotDOMElement(opts) {
+    const rect = await page.evaluate(getDOMElementSize, opts.selector);
+    console.log("TCL: opggHeadless -> rect", rect);
 
-  if (!rect) {
-    throw Error(`Could not find element that matches selector: ${selector}.`);
+    if (!rect) {
+      throw Error(
+        `Could not find element that matches selector: ${opts.selector}.`
+      );
+    }
+
+    await page.screenshot({
+      path: opts.path,
+      clip: {
+        x: rect.left - padding,
+        y: rect.top - padding,
+        width: rect.width + padding * 2,
+        height: rect.height + padding * 2
+      }
+    });
   }
 
-  await page.screenshot({
-    path: "runeExample.png",
-    clip: {
-      x: rect.left - padding,
-      y: rect.top - padding,
-      width: rect.width + padding * 2,
-      height: rect.height + padding * 2
-    }
-  });
-  await browser.close();
+  // screenshot Rune,Item,Spell build on parallel
+  (() => {
+    const promises = [];
+    promises.push(
+      screenshotDOMElement({
+        path: "RuneExample.png",
+        selector: runeQuerySelector
+      })
+    );
+    promises.push(
+      screenshotDOMElement({
+        path: "ItemAndSpellExample.png",
+        selector: itemAndSpellQuerySelector
+      })
+    );
+
+    Promise.all(promises).then(() => {
+      browser.close();
+    });
+  })();
 };
 
 opggHeadless();
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-#lolog puppeteer crawler
+# lolog puppeteer crawler
 
 league of legends 의 챔피언의 공략( 룬, 아이템, 주문) 값을 획득하기위한 크롤러.
