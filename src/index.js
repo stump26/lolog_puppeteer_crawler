cf53ba626985999cfca73940fe76a044f517c501
stump26 <moidpg@gmail.com>
2020-02-07 23:50:36 +0900

    Change data save Function
    새로운 document를 만드는방식에서 기존에 존해하는경우에는 업데이트를 하도록 코드 변경.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const opggHeadless = require("./opggHeadless");
-const getChampionList = require("./getChampionList");
-const { MONGO_URL, DATABASE_NAME } = require("./config");
-const ChampionHintImgModel = require("./mongo/ChampionHintImgModel");
+const opggHeadless = require('./opggHeadless');
+const getChampionList = require('./getChampionList');
+const { MONGO_URL, DATABASE_NAME } = require('./config');
+const ChampionHintImgModel = require('./mongo/ChampionHintImgModel');
 
-console.log(
-  "TCL: (`${MONGO_URL}/${DATABASE_NAME}`",
-  `mongodb://${MONGO_URL}/${DATABASE_NAME}`
-);
+console.log('TCL: (`${MONGO_URL}/${DATABASE_NAME}`', `mongodb://${MONGO_URL}/${DATABASE_NAME}`);
 mongoose.connect(`mongodb://${MONGO_URL}/${DATABASE_NAME}`, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
-mongoose.connection.once("open", () => {
-  console.log("✅MongoDB Connected");
+mongoose.connection.once('open', () => {
+  console.log('✅MongoDB Connected');
 });
 
 const crawChampion = async () => {
   const traverseAll = async cham => {
-    console.log("TCL: traverseAll -> cham", cham);
+    console.log('TCL: traverseAll -> cham', cham);
     const result = await opggHeadless(cham);
-    const newChamDoc = new ChampionHintImgModel({
-      name: cham,
+
+    const query = { name: cham };
+    const update = {
       runeHint: result.runeHint,
-      itemAndSpellHint: result.itemAndSpellHint
+      itemAndSpellHint: result.itemAndSpellHint,
+    };
+    const options = { upsert: true };
+
+    // Find the document
+    ChampionHintImgModel.findOneAndUpdate(query, update, options, (updateError, result) => {
+      if (!updateError) {
+        // If the document doesn't exist, Create it.
+        if (!result) {
+          result = new ChampionHintImgModel({
+            ...query,
+            ...update,
+          });
+        }
+        // Save the document
+        result.save(saveError => {
+          if (saveError) {
+            throw saveError;
+          }
+        });
+      } else {
+        throw updateError;
+      }
     });
     const DB_result = await newChamDoc.save();
-    console.log("TCL: crawChampion -> DB_result", DB_result);
+    console.log('TCL: crawChampion -> DB_result', DB_result);
 
     return DB_result;
   };
 
   const chamList = await getChampionList();
 
-  console.log("TCL: crawChampion -> chamList.length();", chamList.length);
+  console.log('TCL: crawChampion -> chamList.length();', chamList.length);
   chamList
-    .reduce(
-      (chain, item) => chain.then(() => traverseAll(item)),
-      Promise.resolve()
-    )
-    .then(() => console.log("all finished, one after the other"));
+    .reduce((chain, item) => chain.then(() => traverseAll(item)), Promise.resolve())
+    .then(() => console.log('all finished, one after the other'));
 };
 
 crawChampion();
